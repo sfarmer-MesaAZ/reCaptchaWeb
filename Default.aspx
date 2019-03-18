1f1a74676c51bd621b8ce8670c4c86e7100ff0a0
SteveFarmerJr <steve@farmerbrothers.com>
2019-03-18 08:21:18 -0700

    initialize

---
+++ Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="recaptchaWeb._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+  <script type="text/javascript">
+            grecaptcha.ready(function () {
+                grecaptcha.execute('6LdBOpgUAAAAAOwiA35gBcQBjgimSnL_JiC4C5mH', {action: 'test'})
+                .then(function(token) {
+                // Verify the token on the server.
+                    $('#MainContent_captchatoken').val(token);
+                });
+            });
+    </script>
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+        </div>
+        <asp:UpdatePanel runat="server" ID="updatepnl">
+            <ContentTemplate>
+
+                <div id="captchav3" style="margin:0,30px 0 0">
+                    <div class="g-recaptcha" data-sitekey="6Ldi54AUAAAAALp_hS0ipme0fqvPlPzb6O0Mqn8R "style="float:left;clear:left;margin:0 60px 0 90px" ></div>
+                    <asp:Button ID="btnDoAction" runat="server" style="margin-top:20px !important"  
+                        Text="Do Action" CssClass="btn btn-primary" OnClick="btnDispositionReport_Click" 
+                        OnClientClick="ShowStuff()"  />
+                        <br />  
+                <asp:Label runat="server" id="os" name="os" Text="Operating System"/></br>
+                <asp:Label runat="server" id="browser" name="browser" Text="Browser"/></br>
+                <asp:Label runat="server" id="ip" name="ip" Text="IpAddress"/></br>
+                <asp:Label runat="server" id="score" name="score" Text="Score"/></br>
+                </div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+        <div id="the_hidden" style="visibility:hidden;">
+            <asp:Button ID="btnSendToGoog" runat="server" style="margin-top:20px !important;"  
+                Text="Send Result" CssClass="btn btn-primary;"/>
+                <br />  
+
+            <label id="lblCaptchaMessage" runat="server" clientidmode="static"></label> </br>
+            <asp:Textbox runat="server" type="text" id="captchatoken" name="captchatoken" style="visibility:hidden;" />
+        
+            </br>
+        
+        </div>
+        
+
+ <%--           <asp:Label runat="server" id="browser" name="browser" Text="Browser"/>--%>
+
+
+    </div>
+    <script type="text/javascript">
+        function ShowStuff(){
+            $("#the_hidden").css("visibility", "visible");;
+        };
+    </script>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace recaptchaWeb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           // IsReCaptchaValid();
+        }
+
+
+        protected void btnDispositionReport_Click(object sender, EventArgs e)
+        {
+
+            //Validate Captcha
+            var captchaResult = IsReCaptchaValid();
+            lblCaptchaMessage.InnerHtml = captchaResult ? "<span style='color:green'>Captcha verification success</span>" : "<span style='color:red'>Please try your search again in a few seconds.</span>";
+            if (captchaResult)
+            {
+
+            }
+            else { return; }
+        }
+
+
+        [WebMethod]
+        public bool IsReCaptchaValid()
+        {
+            var result = false;
+            var captchaResponse = captchatoken.Text;//Request.Form["g-recaptcha-response"];
+            //
+            var secretKey = "6LdBOpgUAAAAALMCzXpe60f7j-RWDJeVooNbPTq4";//ConfigurationManager.AppSettings["ReCaptchaKey"];
+            var apiUrl = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+            var requestUri = string.Format(apiUrl, secretKey, captchaResponse);
+            var request = (HttpWebRequest)WebRequest.Create(requestUri);
+            //Get browser type for IE exceptions (score = 0.1)
+            string UserAgent = Request.ServerVariables["HTTP_USER_AGENT"];
+            var browserRequest = HttpContext.Current.Request;
+            var browserType = browserRequest.Browser.Type;
+            double captchaScore;
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    JObject jResponse = JObject.Parse(stream.ReadToEnd());
+                    var isSuccess = jResponse.Value<bool>("success");
+                    captchaScore = jResponse.Value<double>("score");
+                    var action = jResponse.Value<string>("action");
+                    if (browserType.Contains("InternetExplorer") | browserType.Contains("Firefox"))
+                    {
+                        result = (isSuccess && captchaScore >= 0.1 && action == "test") ? true : false;
+                    }
+                    else
+                    {
+                        result = (isSuccess && captchaScore >= 0.7 && action == "test") ? true : false;
+                    }
+
+                }
+            }
+            os.Text         = browserRequest.UserHostName;
+            browser.Text    = browserType;
+            ip.Text         = browserRequest.UserHostAddress;
+            score.Text      = captchaScore.ToString();
+            return result;
+        }
+
+    }
+}
